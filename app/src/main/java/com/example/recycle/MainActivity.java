f84baedd27cb0509e30daaf4d27307bdd914217a
jawadiq <jawadiq@users.noreply.github.com>
2021-05-03 09:14:07 +0200

    cc

--- app/src/main/java/com/example/recycle/MainActivity.java
+++ app/src/main/java/com/example/recycle/MainActivity.java
@@ -10,6 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        gjhgjhgjhgjhjhgjh
+
+    }
+    public void eraser (){
+        return;
     }
 }
